11de1b87b1790161ad11b9e4894c14ebaf64bebe
Yoseph <yosephten@gmail.com>
2025-09-06 10:41:42 +0300

    `test`: ✅ modified tests

--- __tests__/book-flow.test.ts
+++ __tests__/book-flow.test.ts
@@ -12,21 +12,26 @@ describe("Full Test", () => {
     book = await fetchBookAction(books[0].id.toString());
   });
 
-  // it("should fetch a book", () => {
-  //   expect(book).toBeTruthy();
-  //   expect(book.title).toBe("Moby Dick; Or, The Whale");
-  // });
+  it("should fetch a book", () => {
+    expect(book).toBeTruthy();
+    expect(book.title).toBe("Moby Dick; Or, The Whale");
+  });
 
-  // it("should get characters from the book", async () => {
-  //   const characters = await getCharacters(book);
-  //   expect(characters).toBeTruthy();
-  //   if (characters) {
-  //     expect(characters.length).toBeGreaterThan(0);
-  //   }
-  // });
+  it("should get characters and their interactions from the book", async () => {
+    const characters = await getCharacters(book);
+    expect(characters).toBeTruthy();
+    if (characters) {
+      console.log(JSON.stringify(characters, null, 4));
+      expect(characters.length).toBeGreaterThan(0);
+    }
+  });
 
   it("should generate avatars for the characters", async () => {
-    const character: Character = { name: "Dracula", description: "A vampire" };
+    const character: Character = {
+      name: "Dracula",
+      description: "A vampire",
+      interactions: [],
+    };
     const avatarUrl = await generateAvatar(character, book);
     expect(avatarUrl).toBeTruthy();
   });
